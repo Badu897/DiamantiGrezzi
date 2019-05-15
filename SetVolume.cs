eff6f699b1755429601be25723bf28388519f54f
EdoardoGuardabascio <edoardo.guardabascio@gmail.com>
2019-05-15 20:50:06 +0200

    Volume

---
+++ SetVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+	public AudioMixer audiomixer;
+
+	public void SetLevel(float volume)
+	{
+		audiomixer.SetFloat("Volume", Mathf.Log10(volume) * 20);
+	}
+}
