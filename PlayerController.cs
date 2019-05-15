e1021f0cbec01823723c8aa4736a155331ddbc8b
EdoardoGuardabascio <edoardo.guardabascio@gmail.com>
2019-05-15 20:50:34 +0200

    Muovi player

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	private Vector3 moveVelocity;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		moveVelocity = moveInput * speed;
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+	}
+}
