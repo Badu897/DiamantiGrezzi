dedc9a9e48c8969ab1d48d0ba253deea2f4cac40
EdoardoGuardabascio <edoardo.guardabascio@gmail.com>
2019-05-15 20:50:00 +0200

    Background

---
+++ Scroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+	public float speed = 0.5f;
+
+	void Update()
+	{
+		Vector2 offset = new Vector2(Time.time * speed, Time.time * speed);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
